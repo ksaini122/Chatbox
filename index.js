954fd24667105d2c691218ac0eeea986bd9fa3d8
Kunal Saini <kunalsaini09102000@gmail.com>
2022-08-18 21:34:43 +0530

    heroku update

--- index.js
+++ index.js
@@ -5,15 +5,10 @@ const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
-const JS = '/js/client.js';
-const CSS = '/css/style.css';
 
 const server = express()
 .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-.use((req, res) => res.sendFile(CSS, { root: __dirname }))
-.use((req, res) => res.sendFile(JS, { root: __dirname }))
-.use((req, res) => res.sendFile('/logo.png.png', { root: __dirname }))
-.use((req, res) => res.sendFile('/tune.mp3.mp3', { root: __dirname }))
+.use(express.static(__dirname + '/public'))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
